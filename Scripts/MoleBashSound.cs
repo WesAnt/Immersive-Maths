cd2ac68a37acef9e9ac5d2d5b40a0466cd46751f
Wesley White <47301142+WesAnt@users.noreply.github.com>
2019-09-29 17:45:00 +0100

    Add files via upload

---
+++ Scripts/MoleBashSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleBashSound : MonoBehaviour {
+    public AudioSource hitSound;
+
+	// Use this for initialization
+	void Start () {
+        hitSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hitSound.Play();
+    }
+}
